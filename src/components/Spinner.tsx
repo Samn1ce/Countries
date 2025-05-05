3b266dc9229ff05272dfbd440a808ee36a743c69
Ifeoluwa <iamsamnice@gmail.com>
2025-05-05 10:45:35 -0700

    [FIX]: Spinner Component

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -4,7 +4,7 @@ export default function Spinner() {
   return (
     <div className="h-screen bg-[#202D36] text-zinc-50">
       <Header />
-      <div className="flex justify-center items-center w-full h-full">
+      <div className="flex justify-center items-center w-full mt-60">
         <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
       </div>
     </div>
